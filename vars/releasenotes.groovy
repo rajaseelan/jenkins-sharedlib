008777e282d6c862dd7ed08dc555973946e2514f
Rajaseelan Ganeswaran <rajaseelan@gmail.com>
2023-10-01 19:57:03 +0100

    first jenkins sharedlib

---
+++ vars/releasenotes.groovy
@@ -0,0 +1,23 @@
+import java.io.*;
+import groovy.io.*;
+
+// format to make it a global function
+def call (Map config=[:]) {
+    def String dir_path = "/home/raja/code/jenkins-go";
+
+    def dir = new File(pwd());
+
+    new File(dir.path + "/releasenotes.txt").withWriter('utf-8')
+    {
+        writer ->
+            dir.eachFileRecurse(FileType.ANY) { file ->
+            if (file.isDirectory()) {
+                writer.writeLine(file.name);
+            }
+            else
+            {
+                writer.writeLine(file.name + '\t' + file.length());
+            }
+        }
+    }
+}
